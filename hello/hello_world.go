58f9d7a80791f5a795e1cfebc84c4cac521769b2
Shinto C  V <cshintov@gmail.com>
2023-03-06 02:44:09 +0530

    Add greetings in Spanish and French

--- hello/hello_world.go
+++ hello/hello_world.go
@@ -3,15 +3,30 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name, lang string) string {
     if name == "" {
         name = "World"
     }
 
-	return englishHelloPrefix + name
+    return greetingPrefix(lang) + name
+}
+
+func greetingPrefix(lang string) string {
+    prefix := englishHelloPrefix
+
+    switch lang {
+    case "French":
+        prefix = frenchHelloPrefix
+    case "Spanish":
+        prefix = spanishHelloPrefix 
+    }
+
+    return prefix
 }
 
 func main() {
-	fmt.Println(Hello("Shinto"))
+	fmt.Println(Hello("Shinto", "English"))
 }
