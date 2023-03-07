277b8986c9c7a2a059bb1e32cfcbb660c3724e7f
Shinto C  V <cshintov@gmail.com>
2023-03-07 11:17:15 +0530

    WIP: finish oop

---
+++ oop/oop_test.go
@@ -0,0 +1,48 @@
+package oop
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+    checkPerimeter := func(t testing.TB, shape Shape, want float64) {
+        t.Helper()
+
+        got := shape.Perimeter()
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+    }
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+        checkPerimeter(t, rectangle, 40.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+        checkPerimeter(t, circle, 62.83185307179586)
+	})
+}
+
+func TestArea(t *testing.T) {
+
+    checkArea := func(t testing.TB, shape Shape, want float64) {
+        t.Helper()
+
+        got := shape.Area()
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+    }
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+        checkArea(t, rectangle, 100.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+        checkArea(t, circle, 314.1592653589793)
+	})
+}
